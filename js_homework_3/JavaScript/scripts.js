8ee89a8febce7fd951c0a1f11ebd07a247d02e56
Klim <shakhray3@gmail.com>
2020-02-23 13:53:32 +0200

    Commited js_hw3_v1

---
+++ js_homework_3/JavaScript/scripts.js
@@ -0,0 +1,42 @@
+let numA;
+let numB;
+let oper;
+
+let previousA;
+let previousB;
+
+do {
+    numA = prompt('Enter first number:', previousA);
+    previousA = numA;
+} while (isNaN(+numA) || numA === '');
+
+
+do {
+    numB = prompt('Enter second number:', previousB);
+    previousB = numB;
+} while (isNaN(+numB) || numB === '');
+
+do {
+    oper = prompt('Enter operator:')
+} while (oper !== '+' && oper !== '-' && oper !== '*' && oper !== '/');
+
+
+function calculation(number1, number2, operator) {
+
+    let res;
+    
+    switch (operator) {
+        case '+': res = +number1 + +number2;
+            break;
+        case '-': res = +number1 - +number2;
+            break;
+        case '*': res = +number1 * +number2;
+            break;
+        case '/': res = +number1 / +number2;
+            break;
+    }
+
+    return res;
+}
+
+console.log(calculation(numA, numB, oper));
