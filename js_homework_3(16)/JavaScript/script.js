dbd8022aede75880c32812f1051cfa4da4db2a39
Klim <shakhray3@gmail.com>
2020-04-21 17:58:30 +0300

    Commit js_hw3(16)_v2.0

--- js_homework_3(16)/JavaScript/script.js
+++ js_homework_3(16)/JavaScript/script.js
@@ -1,14 +1,41 @@
 
+// let n = +prompt('Enter number:');
+//
+// let res;
+//
+// const cache = {};
+//
+// function fibonacciNumber(a) {
+//     if (a in cache) {
+//         return cache[a];
+//     }
+//     if (a <= 1 && a >= -1) {
+//         return a;
+//     }
+//
+//     if (n > 1) {
+//         res = fibonacciNumber(a - 1) + fibonacciNumber(a - 2);
+//     }
+//     else if (n < -1) {
+//         res = fibonacciNumber(a + 1) + fibonacciNumber(a + 2);
+//     }
+//     cache[a] = res;
+//     return res;
+// }
+//
+// alert(fibonacciNumber(n));
+
+
+
+let f0 = 0;
+let f1 = 1;
+
 let n = +prompt('Enter number:');
 
-let res;
-
-const cache = {};
+let res = 0;
 
 function fibonacciNumber(a) {
-    if (a in cache) {
-        return cache[a];
-    }
+
     if (a <= 1 && a >= -1) {
         return a;
     }
@@ -19,8 +46,8 @@ function fibonacciNumber(a) {
     else if (n < -1) {
         res = fibonacciNumber(a + 1) + fibonacciNumber(a + 2);
     }
-    cache[a] = res;
     return res;
 }
 
 alert(fibonacciNumber(n));
+
